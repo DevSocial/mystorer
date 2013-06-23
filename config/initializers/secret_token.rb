c4fc39863b7fb102edd7adc45b0d20bdb9104c27
Gabriel <gabrielrambo_@hotmail.com>
2013-06-23 17:38:45 -0300

    Novo projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystorer::Application.config.secret_token = '5162bf767f116c2eaaede88f46856d50f14a02d232e1c7cc5d9e44a7078d9bd814d398a5fa7c375c00e0d9a0dd5f40cb1c48591eec77b7b640d46769acb9c09c'
